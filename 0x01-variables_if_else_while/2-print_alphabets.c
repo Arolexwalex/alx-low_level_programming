39ce6628e265ddf763bc66566f5859c5fa11385a
Olawale Arowojolu <arolexwalex33@gmail.com>
2022-10-17 14:28:04 -0700

    good coding

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,19 @@
+  #include <studio.h>
+
+  /**
+   * Main - prints the alphabet in lowercase
+   * Return : Alwqys 0
+   */
+
+  int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
