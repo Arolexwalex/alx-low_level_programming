2527ac4b372b304e4e6d3bde65742d8beefabbb9
Olawale Arowojolu <arolexwalex33@gmail.com>
2022-10-18 01:52:56 -0700

    good one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+0-putchar.c char ch;
+
+0-putchar.c ch = 'a';
+
+0-putchar.c while (ch <= 'z')
+0-putchar.c {
+0-putchar.c 0-putchar.c _putchar(ch);
+0-putchar.c 0-putchar.c ch++;
+0-putchar.c }
+
+0-putchar.c _putchar('\n');
+}
