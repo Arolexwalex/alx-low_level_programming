ae2ab439c029b7fe56e87254dbd4e1b6da8efe36
Olawale Arowojolu <arolexwalex33@gmail.com>
2022-10-17 19:38:11 -0700

    good

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prinys the lowercase alphabet in reverse,
+ *
+ * followed by a new line
+ * Return: Always 0
+ */
+init main(void)
+{
+	char ch;
+	for (ch = 'z' ; ch >= 'a' ; ch --)
+	{
+		putcher (ch);
+	}
+	putcher ('\n');
+	return (0);
+}
+
