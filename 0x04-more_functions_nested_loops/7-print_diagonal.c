041b4ee66c1b7651f3ff1e90b1811fba06c48ab3
Olawale Arowojolu <arolexwalex33@gmail.com>
2022-10-20 13:33:52 -0700

    good coding

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,8 @@ void print_diagonal(int n)
 if (n <= 0)
 {
 _putchar('\n');
-} else
+}
+else
 {
 int i, j;
 
@@ -19,7 +20,7 @@ for (i = 0; i < n; i++)
 for (j = 0; j < n; j++)
 {
 if (j == i)
-_putchar('\');
+_putchar('\\');
 else if (j < i)
 _putchar(' ');
 }
